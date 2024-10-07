bb89030adcb99c8cee0de02974923245af8ad60a
JoseSoares645 <jl.soares@campus.fct.unl.pt>
2024-10-08 00:07:45 +0100

    Codigo convertido a partir do diagrama uml feito no staruml

---
+++ src/CourseExecution.java
@@ -0,0 +1,31 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class CourseExecution {
+
+    /**
+     * Default constructor
+     */
+    public CourseExecution() {
+    }
+
+    /**
+     * 
+     */
+    public Employees [] supportedBy;
+
+    /**
+     * 
+     */
+    public Semesters semester;
+
+    /**
+     * 
+     */
+    public int year;
+
+}
---
+++ src/UniversityMember.java
@@ -0,0 +1,31 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class UniversityMember {
+
+    /**
+     * Default constructor
+     */
+    public UniversityMember() {
+    }
+
+    /**
+     * 
+     */
+    public String firstName;
+
+    /**
+     * 
+     */
+    public String lastName;
+
+    /**
+     * 
+     */
+    protected String socialSecurityNumber;
+
+}
